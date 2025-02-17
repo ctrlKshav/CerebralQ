49bb79bfb7e4cb68574f08c88c63ef0778b84a7b
ctrlKshav <keshavm9978@gmail.com>
2025-02-17 17:12:02 +0530

    Accidental Commit

---
+++ app/data/profile.ts
@@ -0,0 +1,18 @@
+﻿import { CheckCircle } from "lucide-react";
+export const features = [
+    {
+      title: "Personalized Insights",
+      description: "See your personality traits unfold.",
+      icon: CheckCircle,
+    },
+    {
+      title: "Visual Breakdown",
+      description: "Interactive charts & graphs.",
+      icon: CheckCircle,
+    },
+    {
+      title: "Famous Personalities",
+      description: "Compare with well-known figures.",
+      icon: CheckCircle,
+    },
+  ];
