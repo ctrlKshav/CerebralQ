85467c35d67bfb6e94d2fc5ee1dd7fc7d19c57fb
ctrlKshav <keshavm9978@gmail.com>
2025-02-17 16:45:26 +0530

    Change MetaData Relevant to Project

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Cerebral Quotient",
+  description: "Discover yourself with tests like MBTI, Big Five, and IQ—all in one place.",
 };
 
 const geistSans = Geist({
