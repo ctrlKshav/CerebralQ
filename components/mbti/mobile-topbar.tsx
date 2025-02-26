24552a6d7d58fce8069f2f28c660b0e74655dbd0
ctrlKshav <keshavm9978@gmail.com>
2025-02-26 18:40:36 +0530

    Swap Logo Place With Section Information for Mobile Topbar

--- components/mbti/mobile-topbar.tsx
+++ components/mbti/mobile-topbar.tsx
@@ -16,15 +16,15 @@ export default function MobileTopbar({
     <div>
       {/* Mobile Topbar */}
       <div className="lg:hidden fixed top-0 left-0 right-0 z-10 dark:bg-gray-800/50 backdrop-blur-sm p-4 flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <div>
+        <div className="order-2">
+          <div className="flex flex-col items-end ">
             <span className="font-bold text-xl">{currentTest.test_name}</span>
             <div className="text-sm text-gray-600 dark:text-gray-400">
               {currentStepText}
             </div>
           </div>
         </div>
-        <Link href="/">
+        <Link href="/" className="order-1">
           <CQLogo className="w-14 h-12" />
         </Link>
       </div>
