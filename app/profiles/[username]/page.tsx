ea06804015ec112e9b1f8e4a473e4db0fe231da8
ctrlKshav <keshavm9978@gmail.com>
2025-02-28 12:18:33 +0530

    Previous Commit Again

--- app/profiles/[username]/page.tsx
+++ app/profiles/[username]/page.tsx
@@ -1,9 +1,10 @@
 ﻿import React from 'react'
+import ProfileInfo from '@/components/profiles'
 
 export default function Page() {
   return (
     <div>
-      
+      <ProfileInfo />
     </div>
   )
 }
