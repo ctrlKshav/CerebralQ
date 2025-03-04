101fa651619388a1fc5bfc4ea1a1c1752ab0045e
ctrlKshav <keshavm9978@gmail.com>
2025-03-04 14:27:15 +0530

    Fix Hyperlink of SignIn and SignUp Forms in Navbar

--- components/navbar/UnprotectedNavbar.tsx
+++ components/navbar/UnprotectedNavbar.tsx
@@ -174,14 +174,14 @@ const UnprotectedNavbar = (props: { className?: string }) => {
                       className="w-full text-base"
                       onClick={() => setIsSheetOpen(false)}
                     >
-                      <Link href="/login">Sign In</Link>
+                      <Link href="/sign-in">Sign In</Link>
                     </Button>
                     <Button
                       variant="default"
                       className="w-full text-base"
                       onClick={() => setIsSheetOpen(false)}
                     >
-                      <Link href="/register">Get Started</Link>
+                      <Link href="/sign-up">Get Started</Link>
                     </Button>
                   </div>
                 </SheetContent>
