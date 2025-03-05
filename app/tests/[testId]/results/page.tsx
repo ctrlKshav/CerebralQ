3f11c7116fc94a1cdb3ec208c5c99e05bff03e0e
ctrlKshav <keshavm9978@gmail.com>
2025-03-05 12:01:42 +0530

    Fix the CQ Logo at Top in Results Page

--- app/tests/[testId]/results/page.tsx
+++ app/tests/[testId]/results/page.tsx
@@ -1,14 +1,41 @@
-﻿import Results from "@/components/results";
+﻿"use client"
+import { useEffect, useState } from "react";
+import Results from "@/components/results";
 import CQLogo from "@/components/cq-logo";
 import Link from "next/link";
 
 export default function ResultsPage() {
+  const [isVisible, setIsVisible] = useState(true);
+  
+  useEffect(() => {
+    const toggleVisibility = () => {
+      // Hide logo when scrolled down more than 100px
+      if (window.scrollY > 100) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
+    
+    window.addEventListener("scroll", toggleVisibility);
+    
+    // Clean up event listener
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+  
   return (
     <div className="">
-      <Link href="/" className="fixed top-0 left-2 sm:top-0 sm:left-10 z-50  ">
-        <CQLogo className="w-36 h-36" />
+      <Link 
+        href="/" 
+        className={`fixed top-0 left-2 sm:top-0 sm:left-10 z-50 transition-opacity  ${
+          isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <CQLogo className="w-24 h-24" />
       </Link>
-      <Results />
+      <div className="">
+        <Results />
+      </div>
     </div>
   );
 }
