0149cee4f1075ff5c55063fa94ed84cfc04f76c8
ctrlKshav <keshavm9978@gmail.com>
2025-03-07 14:06:55 +0530

    Remvoe the params thing from testId page to testId layout

---
+++ app/tests/[testId]/layout.tsx
@@ -0,0 +1,25 @@
+﻿import React from 'react'
+import { use } from 'react'
+import { notFound } from 'next/navigation'
+
+export default function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ testId: string }>;
+}) {
+  const slug = use(params);
+  const testId = slug.testId;
+  
+  // Only show the page if testId is "mbti", otherwise show "not found"
+  if (testId.toLowerCase() !== "mbti") {
+    notFound();
+  }
+  
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
--- app/tests/[testId]/page.tsx
+++ app/tests/[testId]/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/navbar";
 import TestInformation from "@/components/test-info";
 import React from "react";
 import { use } from "react";
-import { notFound } from "next/navigation";
 
 export default function TestInformationPage({
   params,
@@ -11,11 +10,6 @@ export default function TestInformationPage({
 }) {
   const slug = use(params);
   const testId = slug.testId;
-  
-  // Only show the page if testId is "mbti", otherwise show "not found"
-  if (testId.toLowerCase() !== "mbti") {
-    notFound();
-  }
   
   return (
     <>
