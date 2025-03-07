609eba70d7e08964c37fe3feae47eb4d705cd357
ctrlKshav <keshavm9978@gmail.com>
2025-03-07 19:19:27 +0530

    Add Navbar in Results Page

--- app/tests/[testId]/results/page.tsx
+++ app/tests/[testId]/results/page.tsx
@@ -1,39 +1,26 @@
-﻿"use client"
-import { useEffect, useState } from "react";
-import Results from "@/components/results";
-import CQLogo from "@/components/cq-logo";
-import Link from "next/link";
+﻿import Results from "@/components/results";
+import Navbar from "@/components/navbar";
+import { createClient } from "@/utils/supabase/server";
+import { Redirect } from "next";
 
-export default function ResultsPage() {
-  const [isVisible, setIsVisible] = useState(true);
-  
-  useEffect(() => {
-    const toggleVisibility = () => {
-      // Hide logo when scrolled down more than 100px
-      if (window.scrollY > 100) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
+export default async function ResultsPage() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+
+  // Fetch user data including username
+  const { data: userData } = await supabase
+    .from("users")
+    .select("username")
+    .eq("id", user?.id)
+    .single();
     
-    window.addEventListener("scroll", toggleVisibility);
-    
-    // Clean up event listener
-    return () => window.removeEventListener("scroll", toggleVisibility);
-  }, []);
-  
   return (
     <div className="">
-      <Link 
-        href="/" 
-        className={`fixed top-0 left-2 sm:top-0 sm:left-10 z-50 transition-opacity duration-100 ${
-          isVisible ? "opacity-100" : "opacity-0 pointer-events-none sm:opacity-100"
-        }`}
-      >
-        <CQLogo className="w-24 h-24 sm:w-36 sm:h-36" />
-      </Link>
-      <div className="">
+      {user ? <Navbar user={user} username={userData?.username} /> : <Navbar />}
+      <div className="mt-24">
         <Results />
       </div>
     </div>
