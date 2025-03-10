c30f9f2ea26192dcb26f51a03302e87288b787ce
ctrlKshav <keshavm9978@gmail.com>
2025-03-10 19:22:55 +0530

    Redirect from Landing Page to Tests Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import DataVisuals from "@/components/landing-page/data-visuals";
 import Features from "@/components/landing-page/features";
 import { TestsCarousel } from "@/components/landing-page/tests-carousel";
 import Navbar from "@/components/navbar";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  redirect("/tests/mbti")
   return (
     <>
       <Navbar className="" />
