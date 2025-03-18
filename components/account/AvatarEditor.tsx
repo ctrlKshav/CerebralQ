f0fa7d0174a6a14e5939302eb70fe849dbfe886b
ctrlKshav <keshavm9978@gmail.com>
2025-03-18 20:10:01 +0530

    Add Placeholder images instead of avatr

--- components/account/AvatarEditor.tsx
+++ components/account/AvatarEditor.tsx
@@ -32,14 +32,14 @@ export function AvatarEditor({ profileImage, username, userId }: AvatarEditorPro
 
   // Predefined avatar options
   const avatarOptions = [
-    "/avatars/avatar1.png",
-    "/avatars/avatar2.png",
-    "/avatars/avatar3.png",
-    "/avatars/avatar4.png",
-    "/avatars/avatar5.png",
-    "/avatars/avatar6.png",
-    "/avatars/avatar7.png",
-    "/avatars/avatar8.png",
+    "/placeholder.svg",
+    "/placeholder.svg",
+    "/placeholder.svg",
+    "/placeholder.svg",
+    "/placeholder.svg",
+    "/placeholder.svg",
+    "/placeholder.svg",
+    "/placeholder.svg",
   ];
 
   const handleSaveAvatar = async () => {
