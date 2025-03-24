88c85d59af0e89330e7ebba4a22519a47c59b0ee
ctrlKshav <keshavm9978@gmail.com>
2025-03-24 20:04:58 +0530

    Personality Type

--- components/account/index.tsx
+++ components/account/index.tsx
@@ -30,7 +30,7 @@ export default function Account({ userData }: { userData: User }) {
 
         {/* <ProfileProgress /> */}
 
-        {/* <CognitiveMetrics mbti_personality_type={userData.mbti_personality_type} iq={userData.iq} ocean_type={userData.ocean_type} /> */}
+        <CognitiveMetrics mbti_personality_type={userData.mbti_personality_type} iq={userData.iq} ocean_type={userData.ocean_type} />
 
         {/* <AssessmentProgress tests_taken={userData.tests_taken ?? undefined} last_test_date={userData.last_test_date ?? undefined} /> */}
       </div>
