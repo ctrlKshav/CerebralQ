ddbbf902982d033a996117e4720a75ecd3d022fe
ctrlKshav <keshavm9978@gmail.com>
2025-03-25 13:40:19 +0530

    Analytics in Prod only

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,7 @@ const defaultUrl =
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Cerebral Quotient",
-  description:
-    "Discover yourself with MBTI personality test!",
+  description: "Discover yourself with MBTI personality test!",
 };
 
 const geistSans = Geist({
@@ -33,30 +32,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
-      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
 
-      {/* Hotjar */}
-      <Script id="hotjar-script" strategy="afterInteractive">
-        {`
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `}
-      </Script>
+          {/* Hotjar */}
+          <Script id="hotjar-script" strategy="afterInteractive">
+            {`
+      (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    `}
+          </Script>
 
-      {/* Clarity */}
-      <Script id="clarity-script" strategy="afterInteractive">
-        {`  (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "${process.env.CLARITY_ID}");`}
-      </Script>
+          {/* Clarity */}
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`  (function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "${process.env.CLARITY_ID}");`}
+          </Script>
+        </>
+      )}
 
       <body className="bg-background text-foreground">
         <ThemeProvider
