ff796317ea8cd3b4e89d3d861ffe8490ee4c16d0
ctrlKshav <keshavm9978@gmail.com>
2025-03-25 12:52:32 +0530

    Final Global Replace anchor with Link

--- components/navbar/mobile-menu/MobileMenuSectionItem.tsx
+++ components/navbar/mobile-menu/MobileMenuSectionItem.tsx
@@ -1,11 +1,12 @@
 ﻿// components/navbar/MobileLinkItem.tsx
 import React from "react"
 import { MenuSectionItemProps } from "@/types/navbarTypes"
+import Link from "next/link"
 
 export const MobileItem: React.FC<MenuSectionItemProps> = (
   ({ title, href, icon, children, ...props }) => {
   return (
-    <a 
+    <Link
       href={href} 
       className="flex items-center gap-3 py-2 hover:bg-gray-100" 
       target="_blank"
@@ -17,7 +18,7 @@ export const MobileItem: React.FC<MenuSectionItemProps> = (
           {title}
         </div>
       </div>
-    </a>
+    </Link>
   )
 } 
 )
