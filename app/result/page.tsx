bf6c794ab22bca7926895eeb03c573e5cacbbe6a
ctrlKshav <keshavm9978@gmail.com>
2025-03-28 16:23:49 +0530

    Add CQ Logo in Result Page

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,9 +1,13 @@
 ﻿import Result from "@/components/results/result";
+import CQLogo from "@/components/CQLogo";
+import Link from "next/link";
 
 export default async function ResultsPage() {
-
   return (
     <div className="">
+      <Link href="/" className="fixed z-50 top-0 left-4 sm:left-8  ">
+        <CQLogo className="w-24 h-24 sm:w-28 sm:h-28" />
+      </Link>
       <Result />
     </div>
   );
--- app/results/page.tsx
+++ app/results/page.tsx
@@ -3,11 +3,15 @@ import Navbar from "@/components/navbar";
 import { createClient } from "@/utils/supabase/server";
 import { Redirect } from "next";
 import { getCurrentUser } from "@/lib/supabaseOperations";
+import CQLogo from "@/components/CQLogo";
 
 export default async function ResultsPage() {
 
   return (
     <div className="">
+      <div className="">
+        <CQLogo />
+      </div>
       <Results />
     </div>
   );
