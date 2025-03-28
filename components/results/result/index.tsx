dc5d18f5e565c2e27d8ea52c07e5d7fa534b0a7b
ctrlKshav <keshavm9978@gmail.com>
2025-03-28 17:06:17 +0530

    Remove Result Data Saving Logic from Report Page

--- components/results/result/index.tsx
+++ components/results/result/index.tsx
@@ -25,7 +25,6 @@ export default function Result() {
     personalityType,
     personalityDescription,
     completionDate,
-    traitScores,
   } = resultData || sampleResultData;
 
   useEffect(() => {
