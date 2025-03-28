b5674b673b59df61f83f3ee3667b10dafbcb8d7b
ctrlKshav <keshavm9978@gmail.com>
2025-03-28 15:44:18 +0530

    Create useMediaQuery Hook

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,29 @@
+﻿import { useState, useEffect } from 'react';
+
+export function useMediaQuery(query: string): boolean {
+  // Default to false for SSR
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    // Create the media query list
+    const media = window.matchMedia(query);
+    
+    // Set the initial match state
+    setMatches(media.matches);
+    
+    // Define a listener for changes
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    
+    // Add the listener
+    media.addEventListener('change', listener);
+    
+    // Clean up
+    return () => {
+      media.removeEventListener('change', listener);
+    };
+  }, [query]);
+
+  return matches;
+}
