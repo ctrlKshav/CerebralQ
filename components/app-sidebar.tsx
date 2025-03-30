046179d99d3c11a697d43a36aee09e6a8138a069
ctrlKshav <keshavm9978@gmail.com>
2025-03-30 16:43:26 +0530

    Remove Some Useless Components

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -177,12 +177,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
     </Sidebar>
   )
 }
