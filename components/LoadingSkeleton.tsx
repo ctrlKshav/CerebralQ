0161aab8e293dd0d26de13989befaa2b4370acff
ctrlKshav <keshavm9978@gmail.com>
2025-04-07 16:35:03 +0530

    Animate the Loading Skeleton's Loader

--- components/LoadingSkeleton.tsx
+++ components/LoadingSkeleton.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function LoadingSkeleton() {
   return (
     <div className='min-h-screen bg-background flex items-center justify-center'>
-      <Loader2 className='h-10 w-10' />
+      <Loader2 className='h-10 w-10 animate-spin text-primary' />
     </div>
   )
 }
