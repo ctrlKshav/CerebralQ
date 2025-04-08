df37ce0caae7fa0a57253538efc591d439fa1fa8
ctrlKshav <keshavm9978@gmail.com>
2025-04-08 11:59:15 +0530

    Create Layout for Tests Information Page

--- app/tests/[testCode]/page.tsx
+++ app/tests/[testCode]/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-import Navbar from "@/components/navbar";
 import MBTITestInformation from "@/components/test-info/mbti";
 import React, { use } from "react";
-import { useUserDataContext } from "@/context/user-data";
-import LoadingSkeleton from "@/components/LoadingSkeleton";
 import OceanTestInformation from "@/components/test-info/ocean";
 
 export default function TestInformationPage({
@@ -11,24 +8,12 @@ export default function TestInformationPage({
 }: {
   params: Promise<{ testCode: string }>;
 }) {
-  const userDataContext = useUserDataContext();
   const testCode = use(params).testCode;
-
-  if (userDataContext === null) {
-    return null;
-  }
-
-  const { userData, setUserData, loading } = userDataContext;
-
-  if (loading) return <LoadingSkeleton />;
 
   return (
     <>
-      <Navbar user={userData} />
-      <div className="mt-24">
-        {testCode === "mbti" && <MBTITestInformation />}
-        {testCode === "ocean" && <OceanTestInformation />}
-      </div>
+      {testCode === "mbti" && <MBTITestInformation />}
+      {testCode === "ocean" && <OceanTestInformation />}
     </>
   );
 }
---
+++ app/tests/page.tsx
@@ -0,0 +1,8 @@
+import React, { use } from "react";
+
+export default function TestsInformationPage() {
+
+  return (
+   <div className="min-h-screen bg-gray-500 flex justify-center items-center">Hello</div>
+  );
+}
