2e1c4809c0a83ff2d53841eacf88fee699896476
ctrlKshav <keshavm9978@gmail.com>
2025-04-11 17:01:39 +0530

    Preview

--- components/results/report/index.tsx
+++ components/results/report/index.tsx
@@ -13,12 +13,14 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar-custom";
 import { ReportHeader } from "@/components/results/report/SiteHeader";
 import { ReportSidebar } from "@/components/results/report/Sidebar";
 import { User } from "@/types/supabase/users";
+import { PDFViewer } from "@react-pdf/renderer";
+import { PDFResultsDocument } from "@/components/pdf/PDFDocument";
 
 export interface ReportComponentProps {
   userData: User | null;
   resultData: ResultData | null;
 }
-export default function Report({ userData, resultData }: ReportComponentProps) {
+export default function Report({userData, resultData}: ReportComponentProps) {
   const {
     personalityType,
     personalityDescription,
@@ -27,106 +29,9 @@ export default function Report({ userData, resultData }: ReportComponentProps) {
   } = resultData || sampleResultData;
   return (
     <div className="min-h-screen bg-background [--header-height:6rem]">
-      <SidebarProvider className="flex flex-col">
-        <ReportHeader firstname={resultData?.firstname} />
-        <div className="flex flex-1">
-          <ReportSidebar />
-          <SidebarInset>
-            <main className="flex-1 mt-12 mx-auto transition-all duration-300 space-y-8 p-4">
-              {/* Personality Traits */}
-              {traitScores && (
-                <PersonalityTraits
-                  personalityType={personalityType}
-                  traitScores={traitScores}
-                  sectionNumber={1}
-                  firstname={resultData?.firstname ?? undefined}
-                  id="explore-traits"
-                />
-              )}
-
-              {/* Values & Motivators */}
-              <ValuesMotivatorSection
-                firstname={resultData?.firstname || null}
-                valuesAndMotivators={
-                  resultData?.personalityData?.valuesAndMotivators ||
-                  sampleResultData.personalityData?.valuesAndMotivators
-                }
-                sectionNumber={2}
-                id="values-motivators"
-              />
-
-              {/* Career Path */}
-              <CareerPathSection
-                firstname={resultData?.firstname || null}
-                career={
-                  resultData?.personalityData?.career ||
-                  sampleResultData.personalityData?.career
-                }
-                sectionNumber={3}
-                id="career-path"
-              />
-
-              {/* Relationship Insights */}
-              <RelationshipSection
-                firstname={resultData?.firstname || null}
-                relationships={
-                  resultData?.personalityData?.relationships ||
-                  sampleResultData.personalityData?.relationships
-                }
-                sectionNumber={4}
-                id="relationships"
-              />
-
-              {/* Growth Journey */}
-              <GrowthSection
-                firstname={resultData?.firstname || null}
-                growth={
-                  resultData?.personalityData?.growth ||
-                  sampleResultData.personalityData?.growth
-                }
-                sectionNumber={5}
-                id="growth-journey"
-              />
-
-              {/* Daily Habits */}
-              <DailyHabitsSection
-                firstname={resultData?.firstname || null}
-                dailyHabits={
-                  resultData?.personalityData?.dailyHabits ||
-                  sampleResultData.personalityData?.dailyHabits
-                }
-                sectionNumber={6}
-                id="daily-habits"
-              />
-
-              {/* Community Connection */}
-              <CommunitySection
-                firstname={resultData?.firstname ?? null}
-                communityConnection={
-                  resultData?.personalityData.communityConnection ||
-                  sampleResultData.personalityData.communityConnection
-                }
-                sectionNumber={7}
-                id="community"
-              />
-
-              {/* Action Plan */}
-              <ActionPlanSection
-                username={resultData?.username || null}
-                firstname={resultData?.firstname || null}
-                personalityType={personalityType}
-                resultData={resultData || sampleResultData}
-                actionItems={
-                  resultData?.personalityData.actionItems ||
-                  sampleResultData.personalityData.actionItems
-                }
-                sectionNumber={8}
-                id="action-plan"
-              />
-            </main>
-          </SidebarInset>
-        </div>
-      </SidebarProvider>
+      <PDFViewer className="min-h-screen w-full">
+        <PDFResultsDocument resultData={resultData || sampleResultData} />
+      </PDFViewer>
     </div>
   );
 }
