0d70699d4dd352d6e1ce0f94ba77bd7a327dacdd
ctrlKshav <keshavm9978@gmail.com>
2025-04-14 18:37:55 +0530

    Add Ocean and Mbti Folders instead of Dynamic Search Params

---
+++ app/tests/mbti/page.tsx
@@ -0,0 +1,11 @@
+import MBTITestInformation from "@/components/test-info/mbti";
+import React from "react";
+
+export default function TestInformationPage(){
+
+  return (
+    <>
+      <MBTITestInformation />
+    </>
+  );
+}
