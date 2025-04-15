29902eee0f568b929cd90002969d9962ec20e1c4
ctrlKshav <keshavm9978@gmail.com>
2025-04-15 19:10:00 +0530

    Fix Mobile Topbar Active Section Text

--- components/mbti/index.tsx
+++ components/mbti/index.tsx
@@ -157,7 +157,7 @@ export default function MBTITest() {
     }
   };
 
-  const currentStepText = `Section ${currentSectionId} of ${currentTest.sections.length}`;
+  const currentStepText = `Section ${currentSectionId} of ${Object.keys(currentTest.sections).length}`;
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 to-blue-50 dark:from-gray-900 dark:to-gray-800">
