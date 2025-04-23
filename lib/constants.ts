f58c022bae336f778443519b5a7f3ce99e8cb9e3
ctrlKshav <keshavm9978@gmail.com>
2025-04-23 16:30:14 +0530

    Change Reserved Routes

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,7 +4,7 @@ export const TEST_RESULTS_KEY = "cerebralq_mbti_results";
 export const SAVED_RESULTS_KEY = "cerebralq_saved_results";
 export const PROGRESS_KEY = "cerebralq_mbti_progress";
 export const MAX_CHARACTER_LIMIT_BIO = 150;
-export const RESERVED_ROUTES="account,forgot-password,sign-in,sign-up,api,auth,contact,privacy,protected,results,start-test"
+export const RESERVED_ROUTES="account,forgot-password,sign-in,sign-up,api,auth,contact,privacy,protected,results,start-test,ocean,iq,zodiac,astrocartography,tests,test-info,test-history,test-result,images,public,private,cerebralquotient.com,user,test,mbti,elon,Elon,elonmusk,ElonMusk"
 // Define specific colors for icons
 export const ICON_COLORS = {
   award: "#10b981", // emerald-500
