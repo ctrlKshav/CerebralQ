656cb781f7724bad3f943f1cf12ff43910a24d77
ctrlKshav <keshavm9978@gmail.com>
2025-04-23 12:01:57 +0530

    Remove replyto Field because the Freemail and Custom Domain Mismatch was Causing Negative Score

--- app/contact/api/route.ts
+++ app/contact/api/route.ts
@@ -32,7 +32,6 @@ export async function POST(request: Request) {
               MESSAGE:
               ---------------------------
               ${message}`,
-      replyTo: email,
     });
 
     if (error) {
