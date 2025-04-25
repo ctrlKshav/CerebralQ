7a9b95c4045459c429bac6e02dc8c54c80ef17fb
ctrlKshav <keshavm9978@gmail.com>
2025-04-25 13:08:17 +0530

    Create Required Types for Free and Paid Result Data

--- types/tests/mbti/results.ts
+++ types/tests/mbti/results.ts
@@ -1,130 +1,48 @@
 ﻿import { TraitDescription } from "./traits";
-import {  TraitScores } from "./traits";
+import { TraitScores } from "./traits";
+import {
+  PersonalityDescription,
+  RelationshipCompatibility,
+  GrowthJourney,
+  ActionItem,
+  DailyHabits,
+  ValuesAndMotivators,
+  CommunityConnection,
+  CareerPathFull,
+  CareerPathFree,
+} from "./personalityDatabase";
 
-export interface PersonalityDescription {
-  alias: string;
-  description: (firstname: string | null, thirdPerson?: boolean) => string;
-}
-
-export interface PersonalityBlueprint {
-  traits: TraitDescription[];
-  summary: string;
-}
-
-export interface CareerPath {
-  summary: string;
-  superpowers: SuperpowerItem[];
-  growthAreas: GrowthAreaItem[];
-  suggestions: CareerSuggestion[];
-  actionSteps: ActionStepItem[];
-}
-
-export interface CareerSuggestion {
-  title: string;
-  matchPercentage: number;
-  description: string;
-  qualityMatches: CareerQualityMatch[];
-}
-
-export interface CareerQualityMatch {
-  title: string;
-  description: string;
-}
-
-export type RelationshipSectionTitle = "Relationships" | "Friendships"
-
-export interface RelationshipCompatibility {
-  title: RelationshipSectionTitle;
-  subtitle: string;
-  description: string;
-  compatibleTypes: string[];
-  superpowers: SuperpowerItem[];
-  growthAreas: GrowthAreaItem[];
-  actionSteps: ActionStepItem[];
-}
-
-export interface GrowthJourney {
-  summary: string;
-  superpowers: SuperpowerItem[];
-  growthAreas: GrowthAreaItem[];
-  actionSteps: ActionStepItem[];
-}
-
-export interface GrowthMilestone {
-  title: string;
-  description: string;
-  completed: boolean;
-}
-
-export interface DailyHabits {
-  summary: string;
-  habits: {
-    morning: HabitItem;
-    afternoon: HabitItem;
-    evening: HabitItem;
-  };
-  communication: CommunicationStyle;
-}
-
-export interface CommunicationStyle {
-  summary: string;
-  tips: string[];
-}
-
-export interface ValuesAndMotivators {
-  summary: string;
-  coreValues: SuperpowerItem[];
-  motivators: GrowthAreaItem[];
-  actionItems: ActionStepItem[];
-}
-
-export interface CommunityConnection {
-  summary: string;
-  suggestions: string[];
-}
-
-export interface ActionItem {
-  task: string;
-  completed: boolean;
-  description?: string;
-}
-
-export interface SuperpowerItem {
-  title: string;
-  description: string;
-}
-
-export interface GrowthAreaItem {
-  title: string;
-  description: string;
-}
-
-export interface HabitItem {
-  title: string;
-  description: string;
-}
-
-export interface ActionStepItem {
-  number: number;
-  description: string;
-}
-
-export interface ResultData {
+export interface FullResultData {
   username: string | null;
   firstname: string | null;
   personalityType: string;
   personalityDescription: PersonalityDescription;
   completionDate: string;
   traitScores: TraitScores | null;
-  personalityData: PersonalityDatabase;
+  personalityData: FullPersonalityDatabase;
 }
 
-export interface PersonalityDatabase {
-  career: CareerPath;
+export interface FreeResultData {
+  username: string | null;
+  firstname: string | null;
+  personalityType: string;
+  personalityDescription: PersonalityDescription;
+  completionDate: string;
+  traitScores: TraitScores | null;
+  personalityData: FreePersonalityDatabase;
+}
+
+export interface FullPersonalityDatabase {
+  valuesAndMotivators: ValuesAndMotivators;
+  career: CareerPathFull;
   relationships: RelationshipCompatibility[];
   growth: GrowthJourney;
-  actionItems: ActionItem[];
   dailyHabits: DailyHabits;
-  valuesAndMotivators: ValuesAndMotivators;
   communityConnection: CommunityConnection;
+  actionItems: ActionItem[];
+}
+
+export interface FreePersonalityDatabase {
+  valuesAndMotivators: ValuesAndMotivators;
+  career: CareerPathFree;
 }
