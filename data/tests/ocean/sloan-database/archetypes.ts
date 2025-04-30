e2505d29cbd9087fe1307c6c926ae0a4e7ab1ab1
ctrlKshav <keshavm9978@gmail.com>
2025-04-30 19:23:32 +0530

    Create Sloan Archetype

---
+++ data/tests/ocean/sloan-database/archetypes.ts
@@ -0,0 +1,656 @@
+﻿interface SLOANArchetype {
+  code: string;
+  alias: string;
+  description: string;
+  characteristics: string[];
+  traitThresholds: {
+    Extraversion: string;
+    Neuroticism: string;
+    Conscientiousness: string;
+    Agreeableness: string;
+    Openness: string;
+  };
+}
+
+const sloanArchetypes: SLOANArchetype[] = [
+  {
+    code: "SCOAI",
+    alias: "The Leader",
+    description:
+      "Charismatic and stable, you inspire others with your discipline, cooperation, and creativity.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Organized",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SCOAN",
+    alias: "The Mentor",
+    description:
+      "Outgoing and calm, you guide others with your organization, helpfulness, and practical approach.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Organized",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SCOEI",
+    alias: "The Commander",
+    description:
+      "Sociable and confident, you lead with drive, assertiveness, and innovative ideas.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Organized",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SCOEN",
+    alias: "The Director",
+    description:
+      "Social and steady, you manage with goal-oriented focus, independence, and grounded thinking.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Organized",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SCUAI",
+    alias: "The Enthusiast",
+    description:
+      "Energetic and optimistic, you embrace spontaneity, friendliness, and curiosity.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Unstructured",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SCUAN",
+    alias: "The Supporter",
+    description:
+      "Warm and relaxed, you uplift others with flexibility, accommodation, and practicality.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Unstructured",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SCUEI",
+    alias: "The Maverick",
+    description:
+      "Bold and carefree, you challenge norms with impulsiveness, self-reliance, and creativity.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Unstructured",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SCUEN",
+    alias: "The Adventurer",
+    description:
+      "Outgoing and easygoing, you seek freedom with independence and a pragmatic outlook.",
+    characteristics: [
+      "Sociable",
+      "Calm",
+      "Unstructured",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SLOAI",
+    alias: "The Peacemaker",
+    description:
+      "Social and sensitive, you foster harmony with order, empathy, and open-mindedness.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Organized",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SLOAN",
+    alias: "The Caregiver",
+    description:
+      "Friendly and emotional, you nurture with reliability, kindness, and a conventional approach.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Organized",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SLOEI",
+    alias: "The Advocate",
+    description:
+      "Outgoing and expressive, you champion causes with discipline, assertiveness, and creativity.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Organized",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SLOEN",
+    alias: "The Guide",
+    description:
+      "Sociable and passionate, you lead with organization, self-focus, and practical insights.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Organized",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SLUAI",
+    alias: "The Dreamer",
+    description:
+      "Warm and emotional, you inspire with spontaneity, cooperation, and imagination.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Unstructured",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SLUAN",
+    alias: "The Nurturer",
+    description:
+      "Sociable and sensitive, you support with flexibility, kindness, and a grounded perspective.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Unstructured",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "SLUEI",
+    alias: "The Rebel",
+    description:
+      "Expressive and intense, you defy norms with impulsiveness, independence, and innovation.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Unstructured",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "SLUEN",
+    alias: "The Wanderer",
+    description:
+      "Outgoing and emotional, you roam freely with self-reliance and pragmatic thinking.",
+    characteristics: [
+      "Sociable",
+      "Limbic",
+      "Unstructured",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "High",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RCOAI",
+    alias: "The Analyst",
+    description:
+      "Reserved and calm, you excel with organization, cooperation, and curiosity.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Organized",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RCOAN",
+    alias: "The Planner",
+    description:
+      "Quiet and stable, you thrive with discipline, helpfulness, and a practical mindset.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Organized",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RCOEI",
+    alias: "The Strategist",
+    description:
+      "Introverted and confident, you plan with drive, assertiveness, and innovative ideas.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Organized",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RCOEN",
+    alias: "The Builder",
+    description:
+      "Reserved and steady, you create with goal-orientation, independence, and grounded thinking.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Organized",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RCUAI",
+    alias: "The Philosopher",
+    description:
+      "Introspective and optimistic, you explore with flexibility, friendliness, and imagination.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Unstructured",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RCUAN",
+    alias: "The Helper",
+    description:
+      "Quiet and relaxed, you assist with adaptability, accommodation, and practicality.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Unstructured",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RCUEI",
+    alias: "The Individualist",
+    description:
+      "Reserved and carefree, you stand out with spontaneity, self-reliance, and creativity.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Unstructured",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RCUEN",
+    alias: "The Explorer",
+    description:
+      "Introverted and easygoing, you venture with independence and a pragmatic outlook.",
+    characteristics: [
+      "Reserved",
+      "Calm",
+      "Unstructured",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "Low",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RLOAI",
+    alias: "The Mediator",
+    description:
+      "Reserved and sensitive, you balance with order, empathy, and open-mindedness.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Organized",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RLOAN",
+    alias: "The Guardian",
+    description:
+      "Quiet and emotional, you protect with reliability, kindness, and a conventional approach.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Organized",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RLOEI",
+    alias: "The Defender",
+    description:
+      "Introverted and expressive, you uphold with discipline, assertiveness, and creativity.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Organized",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RLOEN",
+    alias: "The Protector",
+    description:
+      "Reserved and passionate, you shield with organization, self-focus, and practicality.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Organized",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "High",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RLUAI",
+    alias: "The Poet",
+    description:
+      "Introspective and emotional, you create with spontaneity, cooperation, and imagination.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Unstructured",
+      "Accommodating",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RLUAN",
+    alias: "The Companion",
+    description:
+      "Quiet and sensitive, you connect with flexibility, support, and a grounded perspective.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Unstructured",
+      "Accommodating",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "High",
+      Openness: "Low",
+    },
+  },
+  {
+    code: "RLUEI",
+    alias: "The Outsider",
+    description:
+      "Reserved and intense, you diverge with impulsiveness, independence, and innovation.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Unstructured",
+      "Egocentric",
+      "Inquisitive",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "High",
+    },
+  },
+  {
+    code: "RLUEN",
+    alias: "The Nomad",
+    description:
+      "Introverted and emotional, you roam with self-reliance and pragmatic thinking.",
+    characteristics: [
+      "Reserved",
+      "Limbic",
+      "Unstructured",
+      "Egocentric",
+      "Non-curious",
+    ],
+    traitThresholds: {
+      Extraversion: "Low",
+      Neuroticism: "High",
+      Conscientiousness: "Low",
+      Agreeableness: "Low",
+      Openness: "Low",
+    },
+  },
+];
