ce6717f2f4ca0bf2ed4597f45662c1ea39625931
ctrlKshav <keshavm9978@gmail.com>
2025-04-30 15:10:40 +0530

    New Form Navigation for Ocean Too

---
+++ components/start-test/ocean/FormNavigation/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+﻿interface ErrorMessageProps {
+  message?: string;
+}
+
+export function ErrorMessage({ message }: ErrorMessageProps) {
+  if (!message) return null;
+  return (
+    <div className="text-sm text-red-500 text-center">
+      {message}
+    </div>
+  );
+}
---
+++ components/start-test/ocean/FormNavigation/SectionAndQuestionCounter.tsx
@@ -0,0 +1,24 @@
+﻿interface SectionAndQuestionCounterProps {
+  currentSection: number;
+  totalSections: number;
+  currentQuestion: number;
+  totalQuestions: number;
+}
+
+export function SectionAndQuestionCounter({
+  currentSection,
+  totalSections,
+  currentQuestion,
+  totalQuestions,
+}: SectionAndQuestionCounterProps) {
+  return (
+    <div className="flex justify-between text-xs text-gray-500 dark:text-gray-400">
+      <span>
+        Section {currentSection} of {totalSections}
+      </span>
+      <span>
+        Question {currentQuestion} of {totalQuestions}
+      </span>
+    </div>
+  );
+}
