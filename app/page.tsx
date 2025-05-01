f5fad598661e1a8df5b109fb40c3da65fd4f21f7
ctrlKshav <keshavm9978@gmail.com>
2025-05-01 12:33:39 +0530

    Fix Protected Navbar Not Showing Up in Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
 import Hero from "@/components/landing-page/Hero";
 import DataVisuals from "@/components/landing-page/DataVisuals";
 import Features from "@/components/landing-page/Features";
-import TestsCarousel  from "@/components/landing-page/TestsCarousel";
+import TestsCarousel from "@/components/landing-page/TestsCarousel";
 import Navbar from "@/components/navbar";
 import Team from "@/components/landing-page/Team";
+import { useUserDataContext } from "@/context/user-data";
+import LoadingSkeleton from "@/components/LoadingSkeleton";
 
-export default async function Home() {
+export default function Home() {
+  const userDataContext = useUserDataContext();
+
+  if (userDataContext === null) {
+    return null;
+  }
+
+  const { userData, loading } = userDataContext;
+
+  if (loading) return <LoadingSkeleton />;
   return (
     <>
-      <Navbar className="" />
+      <Navbar user={userData} />
       <Hero />
       <TestsCarousel />
       <Features />
