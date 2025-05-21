39ed5dbf380de5f6222c0b73c6921095db298d64
ctrlKshav <keshavm9978@gmail.com>
2025-05-21 18:13:40 +0530

    easeOut Animation for Section Change

--- components/mbti/TestForm.tsx
+++ components/mbti/TestForm.tsx
@@ -70,7 +70,7 @@ export function TestForm({
                 initial={{ opacity: 0, x: 20 }}
                 animate={{ opacity: 1, x: 0 }}
                 exit={{ opacity: 0, x: -20 }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.3, ease: "easeOut" }}
                 className="w-full"
               >
                 {sectionQuestions.map((question) => (
