1e5353559acb2e801194da0adaed5da47121af4c
ctrlKshav <keshavm9978@gmail.com>
2025-05-26 13:38:37 +0530

    Refix the Reverse Features

--- components/landing-page/features/FeaturesCard.tsx
+++ components/landing-page/features/FeaturesCard.tsx
@@ -1,6 +1,5 @@
 ﻿"use client";
 
-import { useEffect, useRef } from "react";
 import Image from "next/image";
 import type { Feature } from "@/types/features";
 import { cn } from "@/lib/utils";
@@ -23,26 +22,38 @@ export default function FeatureCard({
     <div
       id={id}
       className={cn(
-        "w-full grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 items-center",
+        "grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12 items-center",
         className
       )}
     >
-      <FeatureContent feature={feature} />
-      <div
-        className={`relative h-[300px] sm:h-[400px] rounded-xl overflow-hidden shadow-lg ${
-          reverse ? "order-1" : "order-2"
-        }`}
-      >
-        <Image
-          src={feature.imageSrc || "/placeholder.svg"}
-          alt={feature.title}
-          fill
-          className="object-contain transition-transform duration-500 hover:scale-105"
-          sizes="(max-width: 768px) 100vw, 50vw"
-          priority={feature.id === 1}
-          quality={100}
-        />
-      </div>
+      {!reverse ? (
+        <>
+          <FeatureContent feature={feature} />
+          <div className="relative h-[300px] sm:h-[400px] rounded-xl overflow-hidden shadow-lg">
+            <Image
+              src={feature.imageSrc || "/placeholder.svg"}
+              alt={feature.title}
+              fill
+              className=" transition-transform duration-500 hover:scale-105"
+              sizes="(max-width: 768px) 100vw, 50vw"
+              priority={feature.id === 1}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="relative h-[300px] sm:h-[400px] rounded-xl overflow-hidden shadow-lg order-1 md:order-none">
+            <Image
+              src={feature.imageSrc || "/placeholder.svg"}
+              alt={feature.title}
+              fill
+              className=" transition-transform duration-500 hover:scale-105"
+              sizes="(max-width: 768px) 100vw, 50vw"
+            />
+          </div>
+          <FeatureContent feature={feature} />
+        </>
+      )}
     </div>
   );
 }
