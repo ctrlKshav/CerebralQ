ef8d2d3a3d3ce446c6fa7c0446420185e4a0ceb1
ctrlKshav <keshavm9978@gmail.com>
2025-05-27 20:44:06 +0530

    Forgot Changing type in a File

--- data/tests/ocean/questions/oceanSample.ts
+++ data/tests/ocean/questions/oceanSample.ts
@@ -1,6 +1,6 @@
-import { OceanTestQuestionsData } from "@/types/tests/testQuestions";
+import { TestQuestionsData } from "@/types/tests/testQuestions";
 
-export const oceanTestQuestionsData: OceanTestQuestionsData = {
+export const oceanTestQuestionsData: TestQuestionsData = {
   id: "6c274f8b-3fd9-4c1e-b20d-7a8c67c2b32a",
   test_name: "OCEAN Personality Test",
   totalQuestions: 20,
