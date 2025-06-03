0d4344eb6d55ddda3df7795cf8c7a5aece9fba3c
ctrlKshav <keshavm9978@gmail.com>
2025-06-03 13:19:51 +0530

    Create Disc Assessment Form

---
+++ app/start-test/disc/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { DISCTestForm } from "@/components/start-test/shared/DISCTestForm";
+import { discGroups } from "@/data/tests/disc/questions/discFull";
+import { DISCResponseSchema } from "@/schema/disc";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, FormProvider } from "react-hook-form";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import DISCTest from '@/components/start-test/tests/disc'
+import React from 'react'
+
+export default function TakeTestpage() {
+  return (
+    <div>
+      <DISCTest/>
+    </div>
+  )
+}
